a9817fb52b703512ebc8b3375a5d27492543a6c0
phamthanhx18 <phamthanhx18@gmail.com>
2023-08-08 11:50:21 +0700

    nap thuc hanh ss05

---
+++ ss05-access-modifier-static/src/practice/StaticProperty/Car.java
@@ -0,0 +1,26 @@
+package practice.StaticProperty;
+
+public class Car {
+
+    private String name;
+
+    private String engine;
+
+
+    public static int numberOfCars;
+
+
+    public Car(String name, String engine) {
+
+        this.name = name;
+
+        this.engine = engine;
+
+        numberOfCars++;
+
+    }
+
+
+    // getters and setters
+
+}
