872655166d335a38964ba56fe2fd8250c8ed00ec
phamthanhx18 <phamthanhx18@gmail.com>
2023-08-18 11:42:15 +0700

    fix khoi tao bien trong vong lap

--- ss14-sort-algorithm/src/exercise/InsertionSort/Main.java
+++ ss14-sort-algorithm/src/exercise/InsertionSort/Main.java
@@ -20,9 +20,11 @@ public class Main {
 
     }
     public static void insertionSort(int[] list) {
+        int key;
+        int j;
         for (int i = 1; i < list.length; ++i) {
-            int key = list[i];
-            int j = i - 1;
+            key = list[i];
+            j = i - 1;
 
             while (j >= 0 && list[j] > key) {
                 list[j + 1] = list[j];
