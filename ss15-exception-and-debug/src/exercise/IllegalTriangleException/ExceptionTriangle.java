4b271c2985a8de9bbc98bcd83f63938283be899e
phamthanhx18 <phamthanhx18@gmail.com>
2023-08-21 11:30:07 +0700

    update

--- ss15-exception-and-debug/src/exercise/IllegalTriangleException/ExceptionTriangle.java
+++ ss15-exception-and-debug/src/exercise/IllegalTriangleException/ExceptionTriangle.java
@@ -1,5 +1,7 @@
 package exercise.IllegalTriangleException;
 
+import java.util.InputMismatchException;
+
 public class ExceptionTriangle extends Exception {
 
     public ExceptionTriangle(String message) {
@@ -10,7 +12,7 @@ public class ExceptionTriangle extends Exception {
         if (a < 0 || b < 0 || c < 0) {
             throw new ExceptionTriangle("Error: Cạnh phải là số dương !, vui lòng thử lại.");
         } else if (a + b <= c || a + c <= b || b + c <= a) {
-            throw new ExceptionTriangle("Error: 2 cạnh không lớn hơn cạnh còn lại !, , vui lòng thử lại");
+            throw new ExceptionTriangle("Error: 2 cạnh không lớn hơn cạnh còn lại !, vui lòng thử lại");
         } else {
             System.out.println("Tạo 3 cạnh của giác hợp lệ");
         }
--- ss15-exception-and-debug/src/exercise/IllegalTriangleException/Main.java
+++ ss15-exception-and-debug/src/exercise/IllegalTriangleException/Main.java
@@ -1,28 +1,30 @@
 package exercise.IllegalTriangleException;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ExceptionTriangle check = new ExceptionTriangle("");
-        int a;
-        int b;
-        int c;
-        do {
-            System.out.print("Nhap vao canh thu nhat: ");
-            a = sc.nextInt();
-            System.out.print("Nhap vao canh thu hai: ");
-            b = sc.nextInt();
-            System.out.print("Nhap vao canh thu ba: ");
-            c = sc.nextInt();
-
             try {
+                int a;
+                int b;
+                int c;
+                System.out.print("Nhap vao canh thu nhat: ");
+                a = sc.nextInt();
+                System.out.print("Nhap vao canh thu hai: ");
+                b = sc.nextInt();
+                System.out.print("Nhap vao canh thu ba: ");
+                c = sc.nextInt();
                 check.checkInputEdge(a, b, c);
                 System.out.println("Bạn vừa nhập tam giác có 3 cạnh là: " + a + "," + b + "," + c);
+            } catch (ExceptionTriangle ex) {
+                System.out.println(ex.getMessage());
+            } catch (InputMismatchException error) {
+                System.out.println("Vui lòng nhập số !");
             } catch (Exception error) {
                 System.out.println(error.getMessage());
             }
-        } while (a < 0 || b < 0 || c < 0 || a + b <= c || a + c <= b || b + c <= a);
     }
 }
