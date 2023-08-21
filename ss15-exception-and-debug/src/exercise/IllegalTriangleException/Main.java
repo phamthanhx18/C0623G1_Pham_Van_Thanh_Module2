7dd977472626027a0a24c5aafe2cadbe0541a765
phamthanhx18 <phamthanhx18@gmail.com>
2023-08-21 15:14:54 +0700

    update do while

--- ss15-exception-and-debug/src/exercise/IllegalTriangleException/Main.java
+++ ss15-exception-and-debug/src/exercise/IllegalTriangleException/Main.java
@@ -1,30 +1,55 @@
 package exercise.IllegalTriangleException;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ExceptionTriangle check = new ExceptionTriangle("");
+        ExceptionTriangle exTriangle = new ExceptionTriangle("");
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        do {
             try {
-                int a;
-                int b;
-                int c;
-                System.out.print("Nhap vao canh thu nhat: ");
-                a = sc.nextInt();
-                System.out.print("Nhap vao canh thu hai: ");
-                b = sc.nextInt();
-                System.out.print("Nhap vao canh thu ba: ");
-                c = sc.nextInt();
-                check.checkInputEdge(a, b, c);
-                System.out.println("Bạn vừa nhập tam giác có 3 cạnh là: " + a + "," + b + "," + c);
-            } catch (ExceptionTriangle ex) {
-                System.out.println(ex.getMessage());
-            } catch (InputMismatchException error) {
-                System.out.println("Vui lòng nhập số !");
-            } catch (Exception error) {
-                System.out.println(error.getMessage());
+                do {
+                    try {
+                        System.out.print("Nhập giá trị của cạnh thứ nhất: ");
+                        a = Integer.parseInt(sc.nextLine());
+                        if (a <= 0) {
+                            System.out.println("Vui lòng nhập số lớn hơn 0");
+                        }
+                    } catch (NumberFormatException exception) {
+                        System.out.println("Bạn cần nhập số !");
+                    }
+                } while (a <= 0);
+
+                do {
+                    try {
+                        System.out.print("Nhập giá trị của cạnh thứ hai: ");
+                        b = Integer.parseInt(sc.nextLine());
+                        if (b <= 0) {
+                            System.out.println("Vui lòng nhập số lớn hơn 0");
+                        }
+                    } catch (NumberFormatException exception) {
+                        System.out.println("Bạn cần nhập số !");
+                    }
+                } while (b <= 0);
+
+                do {
+                    try {
+                        System.out.print("Nhập giá trị của cạnh thứ ba: ");
+                        c = Integer.parseInt(sc.nextLine());
+                        if (c <= 0) {
+                            System.out.println("Vui lòng nhập số lớn hơn 0");
+                        }
+                    } catch (NumberFormatException exception) {
+                        System.out.println("Bạn cần nhập số !");
+                    }
+                } while (c <= 0);
+                exTriangle.checkInputEdge(a, b, c);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
+        } while (a + b <= c || a + c <= b || b + c <= a);
     }
 }
